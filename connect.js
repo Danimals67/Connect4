dab09592ef02921727c8f503c7966b551ee87b14
Daniel <dfparson97@gmail.com>
2016-07-05 10:47:09 -0600

    Connect4

---
+++ connect.js
@@ -0,0 +1,261 @@
+
+window.onload = drawBoard;
+//ADD PIECES TO 2D ARRAY USING COLUMN AND ROW DATA TO SPECIFY WHERE IN THE ARRAY THE PIECE GOES
+//http://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938
+//http://inventwithpython.com/blog/2012/02/20/i-need-practice-programming-49-ideas-for-game-clones-to-code/
+var canvas;
+var ctx;
+var pieceX    = 170;
+var pieceY    = 50;;
+var column    = 0;
+var board     = new Array(7);
+var move      = 1;
+var rowCount = 0;
+var colCount = 0;
+
+function drawBoard(){
+	for(var i = 0; i < 7; i++){
+		board[i] = new Array(7);
+	}
+	canvas = document.getElementById("myCanvas");
+	canvas.addEventListener("click", clickPos, false);
+	ctx = canvas.getContext("2d")
+	
+	//works for 900x1440 does not work for 900x1600
+	/*canvas.width = window.innerWidth / 1.6;
+	canvas.height = window.innerHeight / 1.2;*/
+	
+	canvas.style.backgroundColor = "#E6EDFF";
+	while(colCount < 6){
+		while(rowCount < 7){
+			drawPiece();
+			pieceX += 110;
+			rowCount++
+		}
+		colCount++
+		pieceY += 100;
+		pieceX = 170;
+		rowCount = 0;
+	}
+	
+}
+
+function ResetBoard(){
+	pieceX = 170;
+    pieceY = 25;
+	rowCount = 0;
+	colCount = 0;
+	drawBoard();
+}
+
+function drawPiece(){
+	ctx.fillStyle = "#1770FF";
+	//alert("redraw");
+	ctx.beginPath();
+	ctx.arc(pieceX - 50, pieceY + 50, 40, 0, 2 * Math.PI);
+	ctx.rect(pieceX, pieceY, -100, 100);
+	ctx.fill();
+	
+}
+
+function clickPos(event){
+	var totalOffsetX   = 0;
+    var totalOffsetY   = 0;
+    var canvasX        = 0;
+    var canvasY        = 0;
+    var currentElement = this;
+	var increment      = 70;
+	var hundo          = 0;
+
+    do{
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+	/*if(70 == Math.abs(070)){
+		alert("True");
+	}*/
+	//FIXED
+	//BUG for some reason for loop is looping once before recognizing first column
+	//alert("X: " + canvasX + "   " + "Y: " + canvasY);
+	for(i = 0; i < 7; i++){
+		if(i == 0){
+			if(canvasX >= increment && canvasX <= (increment + 100)){
+				column = (i + 1);
+				//alert("Column " + column + " \n MIN: " + increment + "\n MAX: " + (increment + 100));
+				Move();
+			}
+			increment += 10;
+			hundo += 100;
+		}
+		else if(i > 0){
+			if(canvasX >= (hundo + increment) && canvasX <= ((hundo + 100) + increment)){
+				column = (i + 1);
+				//alert("Column " + column + " \n MIN: " + (hundo + increment) + "\n MAX: " + ((hundo + 100) + increment));
+				//increment += 10;
+				Move();
+			}
+			increment += 10;
+			hundo += 100;
+		}
+		//alert("Column " + (i + 1) + " \n MIN: " + (hundo + increment) + "\n MAX: " + ((hundo + 100) + increment));
+		//increment += 10;
+	}
+}
+
+function Move(){
+	if(move == 1){
+		redMove();
+	}
+	else
+		yellowMove();
+}
+
+function redMove(){
+	var yCirc = 600;
+	/*var rowCorrector = 5;
+	var rowS = String(yCirc).charAt(0);
+	var row = Number(rowS) - rowCorrector;*/
+	var row = 0;
+	var col = column - 1;
+	
+	for(i = 0; i < 5; i++){
+		var imgd = ctx.getImageData(column + '' + (column + 1) + '' + 0, yCirc, canvas.width, canvas.height);
+		var pix = imgd.data;
+		if(pix[0] == 255 || (pix[0] == 255 && pix[1] == 255)){
+			//alert("RED");
+			row++;
+			yCirc -= 100;
+			/*rowCorrector--;
+			rowS = String(yCirc).charAt(0);
+			row = Number(rowS) - rowCorrector;*/
+		}
+	}
+	ctx = canvas.getContext("2d")
+	ctx.beginPath();
+	ctx.arc(column + '' + (column + 1) + '' + 0, yCirc, 40, 0, 2 * Math.PI);
+	ctx.fillStyle = "Red";
+	ctx.fill();
+	board[col][row] = "R";
+	
+	//alert("Red Move in column " + column);
+	move = 2;
+	document.getElementById("Turn").innerHTML = "Turn: Yellow";
+	//alert("Column " + col + "  " + "Row " + row);
+	//alert(board[1][2])
+	//checkWin();
+	checkWin();
+}
+
+function yellowMove(){
+	//alert("Yellow Move in column " + column);
+	var yCirc = 600;
+	var row = 0;
+	var col = column - 1;
+	
+	for(i = 0; i < 5; i++){
+		var imgd = ctx.getImageData(column + '' + (column + 1) + '' + 0, yCirc, canvas.width, canvas.height);
+		var pix = imgd.data;
+		if(pix[0] == 255 || (pix[0] == 255 && pix[1] == 255)){
+			//alert("YELLOW");
+			row++;
+			yCirc -= 100;
+		}
+	}
+	
+	ctx = canvas.getContext("2d")
+	ctx.beginPath();
+	ctx.arc(column + '' + (column + 1) + '' + 0, yCirc, 40, 0, 2 * Math.PI);
+	ctx.fillStyle = "Yellow";
+	ctx.fill();
+	board[col][row] = "Y";
+	move = 1;
+	document.getElementById("Turn").innerHTML = "Turn: Red";
+}
+
+function TestValue(){
+	var xBoard = document.getElementById("xCoord").value;
+	var yBoard = document.getElementById("yCoord").value;
+	alert(board[xBoard][yBoard]);
+}
+
+function checkHorizontal(){
+	var win = false;
+	var concRow = "";
+	var index = 0;
+	for(i = 0; i < 7; i++){
+		for(var j = 0; j < 1; j++){
+			concRow += board[i][j];
+		}
+	}
+	for(i = 0; i < 7; i++){
+		if(concRow.indexOf("R", i)){
+			alert("Blah");
+		}
+	}
+}
+
+function checkWin(){
+	var start;
+	for(var i = 0; i < 7; i++){
+		for(var j = 0; j < 6; j++){
+			if(board[i][j] == "R"){
+				/*if(i => 3){
+					//checkLeft
+					if(board[i - 1][j] == "R"){
+						if(board[i - 2][j] == "R"){
+							if(board[i - 3][j] == "R"){
+								alert("Win");
+							}
+						}
+					}
+				}*/
+				//I don't know how this is working
+				//HORIZONTAL CHECK
+				if(i <= 3){
+					//checkRight
+					if(board[i + 1][j] == "R"){
+						if(board[i + 2][j] == "R"){
+							if(board[i + 3][j] == "R"){
+								alert("Horizonal Red Win");
+							}
+						}
+					}
+				}
+				//VERTICAL CHECK
+				if(j <= 4){
+					if(board[i][j + 1] == "R"){
+						if(board[i][j + 2] == "R"){
+							if(board[i][j + 3] == "R"){
+								alert("Vertical Red Win");
+							}
+						}
+					}
+				}
+				//UP-RIGHT DIAGONAL CHECK
+				if(i <= 3 && j <= 4){
+					if(board[i+1][j+1] == "R"){
+						if(board[i+2][j+2] == "R"){
+							if(board[i+3][j+3]){
+								alert("Up-Right Red Win");
+							}
+						}
+					}
+				}
+				//UP-LEFT DIAGONAL CHECK
+				if(i <=3 && j <= 4){
+					if(board[i-1][j+1] == "R"){
+						if(board[i-2][j+2] == "R"){
+							if(board[i-3][j+3]){
+								alert("Up-Left Red Win");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
